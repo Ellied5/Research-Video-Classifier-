9f542e6a6e01bba3430cb99873a3c85986436e26
Ellied5 <ellfdawson@gmail.com>
2022-06-27 10:08:43 -0400

    Update process_video.py

--- process_video.py
+++ process_video.py
@@ -16,16 +16,29 @@ ass = []
 
 # torch.Size([40, 540, 960, 3])
 
-for i in range(1,50):
+for i in range(1,170):
 	if i != 2:
 		continue
-	fv = "Video_Clips_trim/"+str(i)+".mp4"
+	fv = "Individual_clips/Post-Injury/T("+str(i)+").mp4"
 	dt,_,_ = torchvision.io.read_video(fv)
 	#dt = dt.permute(0, 3, 1, 2)
 	#print(dt.shape)
 	#for j in range(dt.shape[0]):
 	#	imgj = dt[j,:,:,:]
 	#	print(imgj.shape)
-	#	save_image(imgj, 'data/'+str(j)+'.png')
+	#	save_image(imgj, 'data/T'+str(j)+'.png')
+    
+for i in range(1,662):
+	if i != 2:
+		continue
+	fv = "Individual_clips/Pre-Injury/R("+str(i)+").mp4"
+	dt,_,_ = torchvision.io.read_video(fv)
+	#dt = dt.permute(0, 3, 1, 2)
+	#print(dt.shape)
+	#for j in range(dt.shape[0]):
+	#	imgj = dt[j,:,:,:]
+	#	print(imgj.shape)
+	#	save_image(imgj, 'data/R'+str(j)+'.png')
+
 
 
