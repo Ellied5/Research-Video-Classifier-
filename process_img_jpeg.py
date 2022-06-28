6808e2696b50c2ed232724d567383457b0f1cb92
Ellied5 <106102262+Ellied5@users.noreply.github.com>
2022-06-28 17:15:00 -0400

    Create process_img_jpeg.py

---
+++ process_img_jpeg.py
@@ -0,0 +1,52 @@
+import sys
+import torch
+import torchvision
+import numpy as np
+import torch.nn.functional as F
+from torchvision.utils import save_image
+from torchvision.io import write_jpeg
+from torchvision import transforms
+from torchvision.transforms.functional import to_tensor
+from PIL import Image
+import random
+import os 
+
+
+data_transforms = transforms.Compose([
+    transforms.Resize((256, 256)),transforms.ToTensor(),
+		transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),])
+trans=transforms.ToPILImage()
+trans2=transforms.PILToTensor()
+ass = []
+
+torch.Size([40, 540, 960, 3])
+
+for i in range(1,171):
+	file="T ("+str(i)+").mp4"
+	fv ="Individual_clips/Post-Injury/"+file 
+	dt,_,_ = torchvision.io.read_video(fv)
+	dt = dt.permute(0, 3, 1, 2)
+	l = random.sample(range(dt.shape[0]), 20)
+	for j in range(len(l)):
+		imgj = dt[l[j],:,:,:]
+		imgj=trans(imgj)
+		imgj=data_transforms(imgj)
+		newpath = 'data/Post-Injury/'+str(i)+'/' 
+		if not os.path.exists(newpath):
+			os.makedirs(newpath)
+		save_image(imgj, newpath+str(j)+'.png')
+        
+for i in range(1,663):
+	file="R("+str(i)+").mp4"
+	fv ="Individual_clips/Pre-Injury/"+file 
+	dt,_,_ = torchvision.io.read_video(fv)
+	dt = dt.permute(0, 3, 1, 2)
+	l = random.sample(range(dt.shape[0]), 20)
+	for j in range(len(l)):
+		imgj = dt[l[j],:,:,:]
+		imgj=trans(imgj)
+		imgj=data_transforms(imgj)
+		newpath = 'data/Pre-Injury/'+str(i)+'/'
+		if not os.path.exists(newpath):
+			os.makedirs(newpath)
+		save_image(imgj, newpath+str(j)+'.png')
